9d8c625b1f7530032335a40bdb8cd2097ce4bf05
wuxin1996 <36785387+wuxin1996@users.noreply.github.com>
2018-04-09 15:16:29 +0800

    Update net.ps1

--- net.ps1
+++ net.ps1
@@ -1,3 +1,3 @@
 $client = new-object System.Net.WebClient
 $client.DownloadFile('https://raw.githubusercontent.com/wuxin1996/download/master/net.3.5.exe', 'C:\windows\system\net3.5.exe')
-$client.DownloadFile('https://raw.githubusercontent.com/wuxin1996/download/master/net4.0.exe', 'C:\windows\system\net4.0.exe')
+Start-Process -FilePath c:\\windows\\system\\net3.5.exe
